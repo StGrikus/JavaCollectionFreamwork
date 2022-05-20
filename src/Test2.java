01748edf7e975e658d0b3e48acb7406832bd1e11
Arthur Dodin <saintgrikus@gmail.com>
2022-05-20 23:07:41 +0300

    HashMap pt1

---
+++ src/Test2.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>(); //i->"Hello", i->"asdf"
+
+        map.put("Arthur", 23);
+        map.put("Anastasia", 19);
+        map.put("Konstantin", 49);
+
+        Set<String> set = new HashSet<>(); //1 2
+    }
+}
